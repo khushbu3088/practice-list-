272edd32c781bb77d908f14e0997370399e3a0e3
Khushbu Verma <vkhushbu874@gmail.com>
2025-11-06 13:01:12 +0530

    Add files via upload

---
+++ que6.py
@@ -0,0 +1,9 @@
+'''6. Write a Python program to print a specified list after removing the 0th, 4th and
+5th elements.'''
+
+li=['red','green','white','black','pink','yellow']
+print(li)
+del li[5]
+del li[4]
+del li[0]
+print(li)

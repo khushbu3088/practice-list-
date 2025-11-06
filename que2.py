ea4ebaf166ff8b5b4caffa78da6eedfe6bbc442e
Khushbu Verma <vkhushbu874@gmail.com>
2025-11-06 12:59:27 +0530

    Add files via upload

---
+++ que2.py
@@ -0,0 +1,9 @@
+#2. Write a Python program to get the largest number from a list
+'''li=[30,40,55,67,88]
+largest_num=max(li)
+print(largest_num)'''
+
+#2 wy
+li=[30,40,55,67,88]
+asc_ord=sorted(li)
+print(asc_ord[-1])

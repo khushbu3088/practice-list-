40a4c999dea4de7757b4c2a7a809299eef70b278
Khushbu Verma <vkhushbu874@gmail.com>
2025-11-06 13:00:48 +0530

    Add files via upload

---
+++ que5.py
@@ -0,0 +1,13 @@
+#5. Write a Python program to check a list is empty or not.
+li=[]
+if li==[]:
+    print("empty li")
+else:
+    print("not empty")
+#2 wy
+li=[]
+print(li)
+if len(li)==0:
+    print("empty list")
+else:
+    print("not empty")
